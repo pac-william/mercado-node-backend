f1df872768fcc05f920d1445cb70c2b2ddc9ce30
romulogdonadoni <romulogdonadoni@gmail.com>
2025-09-04 12:51:26 -0300

    Add metaSchemas definition for pagination metadata in meta.ts

---
+++ src/utils/schema/meta.ts
@@ -0,0 +1,12 @@
+export const metaSchemas = {
+    "Meta": {
+        "type": "object",
+        "properties": {
+            "page": { "type": "integer", "example": 1 },
+            "size": { "type": "integer", "example": 10 },
+            "total": { "type": "integer", "example": 100 },
+            "totalPages": { "type": "integer", "example": 10 },
+            "totalItems": { "type": "integer", "example": 1000 }
+        }
+    }
+}; 
