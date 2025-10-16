9ef460b250c84c2d0e1132cc1c418ea05c7f8806
romulogdonadoni <romulogdonadoni@gmail.com>
2025-10-16 16:13:03 -0300

    feat: refactor Elasticsearch query construction to handle optional category filtering

--- src/rest/productElasticSearch.ts
+++ src/rest/productElasticSearch.ts
@@ -6,31 +6,56 @@ class ProductElasticSearch {
     categoryName: string // categoria que vem do banco
   ) {
     // Monta a query do Elasticsearch
-    const esQuery = {
-      from: (page - 1) * size,
-      size: size,
-      query: {
-        bool: {
-          must: [
-            {
-              match: {
-                name: {
-                  query: productName,
-                  fuzziness: "AUTO" // apenas para nome do produto
+
+    let esQuery;
+
+    if (categoryName) {
+      esQuery = {
+        from: (page - 1) * size,
+        size: size,
+        query: {
+          bool: {
+            must: [
+              {
+                match: {
+                  name: {
+                    query: productName,
+                    fuzziness: "AUTO" // apenas para nome do produto
+                  }
                 }
               }
-            }
-          ],
-          filter: [
-            {
-              term: {
-                "categoryName.keyword": categoryName // filtro exato
+            ],
+            filter: [
+              {
+                term: {
+                  "categoryName.keyword": categoryName // filtro exato
+                }
               }
-            }
-          ]
+            ]
+          }
         }
-      }
-    };
+      };
+
+    } else {
+      esQuery = {
+        from: (page - 1) * size,
+        size: size,
+        query: {
+          bool: {
+            must: [
+              {
+                match: {
+                  name: {
+                    query: productName,
+                    fuzziness: "AUTO" // apenas para nome do produto
+                  }
+                }
+              }
+            ]
+          }
+        }
+      };
+    }
 
     // Chamada ao Elasticsearch
     const response = await fetch(`${process.env.ELASTICSEARCH_URL}/produtos/_search`, {
