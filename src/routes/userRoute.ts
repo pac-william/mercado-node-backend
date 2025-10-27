ec44c3b7385d728d270899dc924988f9034d9a0a
romulogdonadoni <romulogdonadoni@gmail.com>
2025-10-27 05:27:01 -0300

    refactor: update user routes configuration

--- src/routes/userRoute.ts
+++ src/routes/userRoute.ts
@@ -5,10 +5,11 @@ import { validateToken } from '../middleware/validateToken';
 const router = Router();
 
 // Rotas públicas
+router.post('/', userController.createUser);
+
+// Privadas
 router.get('/', validateToken, userController.getUsers);
 router.get('/:id', validateToken, userController.getUserById);
-// Privadas
-router.post('/', validateToken, userController.createUser);
 router.put('/:id', validateToken, userController.updateUser);
 router.patch('/:id', validateToken, userController.updateUserPartial);
 router.delete('/:id', validateToken, userController.deleteUser);
